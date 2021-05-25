4458505d85853a374bcca1ee042977bdaf4db121
Springhser <chenjinbincjb@163.com>
2021-05-25 21:27:01 +0800

    add base data structure

---
+++ Point.hpp
@@ -0,0 +1,24 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: springhser
+ * @Date: 2021-05-25 20:50:48
+ * @LastEditors: springhser
+ * @LastEditTime: 2021-05-25 21:02:03
+ */
+#ifndef POINT_HPP
+#define POINT_HPP
+
+#include <vector>
+
+struct Point2D
+{
+    Point2D(double px, double py):x(px), y(py){}
+    double x;
+    double y;
+}
+
+using Points = std::vector<Point2D>;
+
+
+#endif
